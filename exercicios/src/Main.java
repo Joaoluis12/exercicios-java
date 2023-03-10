c2cd2ebdc0d2e5dd7a70e3ebd620bbd2be8c11f4
Joaoluis12 <joaoluis.lourenco12@gmail.com>
2023-03-10 11:18:11 -0300

    criando projeto

--- exercicios/src/Main.java
+++ exercicios/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int num;
+        Scanner ler = new Scanner(System.in);
+        imprimir("Digite um numero: ");
+        num = ler.nextInt();
+        String result = num%2==0 ? "Número Par" : "Número Impar";
+        imprimir(result);
+    }
+
+    public static void imprimir(String texto) {
+        System.out.println(texto);
     }
 }
+
+
+
